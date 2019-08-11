e75d5e52dbc3f62825161b265cec4bacbd83c67f
Timo Anttila <tewdin@users.noreply.github.com>
2019-08-11 10:43:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
+<!doctype html>
+<html amp lang="en">
+<head><meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+<title><?= $title_head ?></title>
+<meta name="format-detection" content="telephone=no">
+<link rel="icon" type="image/png" href="https://timoanttila.com/favicon.png">
+<link rel="icon" href="https://timoanttila.com/favicon.ico" type="image/x-icon">
+<script async src="https://cdn.ampproject.org/v0.js"></script>
 <script async custom-element="amp-bind" src="https://cdn.ampproject.org/v0/amp-bind-0.1.js"></script>
+<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+<style amp-custom>
+  Add some styles
+</style>
+<link rel="canonical" href="realurl">
+</head><body>
+
 <button id="menu-toggler" class="icon icon-menu" on="tap:menu.toggleClass(class='s1')"></button>
 <nav id='menu'>
   <ul>
@@ -22,3 +38,5 @@
     <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
   </ul>
 </nav>
+
+</body></html>
